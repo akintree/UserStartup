9cdbaa0e485247bf5cb263c39974ed3976f44292
Annie Kintree <akintree53@gmail.com>
2020-03-30 20:26:53 -0500

    Add project files.

---
+++ UserStartup/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserStartup.Models;
+using UserStartup.ViewModels;
+
+namespace UserStartup.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()   
+        {
+            List<User> users = AddUserViewModel.UserList;
+            return View(users);
+        }
+        
+        public IActionResult Add()
+        {
+            User user = new User();
+            ViewBag.Title = "Add User";
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {           
+            if (ModelState.IsValid)
+            {
+                User newUser = new User
+                {
+                    Username = user.Username,
+                    Email = user.Email,
+                    Password = user.Password
+                };
+                if (newUser.Password is null)
+                {
+                    ViewBag.error = "Please enter a password";
+                    throw ViewBag.error;
+                } else
+                {
+                    if (verify != newUser.Password)
+                    {
+                        ViewBag.error = "Passwords need to match";
+                        throw ViewBag.error;
+                    };
+                    AddUserViewModel.UserList.Add(newUser);
+                    return Redirect("/User");
+                }
+            }
+            return View(user);
+        }
+    }
+}
---
+++ UserStartup/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserStartup.Models;
+
+namespace UserStartup.ViewModels
+{
+    public class AddUserViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string VerifyPassword { get; set; }
+        
+        public static List<User> UserList  { get; set; }
+
+        //public void AddUser(User newUser)
+        //{
+        //    UserList.Add(newUser);
+        //}
+    }
+}
